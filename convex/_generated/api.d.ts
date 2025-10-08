a030c33891d7e9e1dc1dd055b84cb5a251b9d5c7
hmsalves550-dot <hmsalves550@gmail.com>
2025-10-08 16:56:27 +0100

    Add files via upload

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,48 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+import type * as admin from "../admin.js";
+import type * as auth from "../auth.js";
+import type * as carousel from "../carousel.js";
+import type * as http from "../http.js";
+import type * as logos from "../logos.js";
+import type * as pricing from "../pricing.js";
+import type * as router from "../router.js";
+
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+declare const fullApi: ApiFromModules<{
+  admin: typeof admin;
+  auth: typeof auth;
+  carousel: typeof carousel;
+  http: typeof http;
+  logos: typeof logos;
+  pricing: typeof pricing;
+  router: typeof router;
+}>;
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
